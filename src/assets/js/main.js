a131d86fff432041c1c5fadc60d83fc806944918
ics-ikeda <ikeda@ics-web.jp>
2026-04-17 21:35:03 +0900

    不要な演出と依存を削除してメディアクエリを整理する

--- src/assets/js/main.js
+++ src/assets/js/main.js
@@ -1,9 +1,8 @@
-import { SimpleScrollTrigger } from "simple-scroll-trigger";
-
 document.addEventListener("DOMContentLoaded", () => {
   let isHeaderActive = false;
   const hamburgerButton = document.querySelector(".hamburger-button");
   const header = document.querySelector(".header");
+
   hamburgerButton.addEventListener("click", () => {
     isHeaderActive = !isHeaderActive;
     if (isHeaderActive) {
@@ -13,20 +12,5 @@ document.addEventListener("DOMContentLoaded", () => {
       header.classList.remove("is-active");
       hamburgerButton.classList.remove("is-active");
     }
-  });
-  const portraitElement = document.querySelector(".missions__portrait");
-
-  new SimpleScrollTrigger({
-    trigger: portraitElement,
-    onEnter: () => {
-      const photo = document.querySelector(".missions__portrait-photo");
-      if (!photo.classList.contains(".is-show")) {
-        document
-          .querySelector(".missions__portrait-photo")
-          .classList.add("is-show");
-      }
-    },
-    startViewPortPoint: { value: 70, unit: "%" },
-    startTriggerPoint: { value: 20, unit: "%" },
   });
 });
